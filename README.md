f84c3442bec14f2fc15b286de00f81ff9394138c
Flora-San <florahsanchez@gmail.com>
2022-11-07 17:35:32 +0100

    bot

---
+++ README.md
@@ -0,0 +1,48 @@
+simply configurations
+
+[PyCharm](https://www.jetbrains.com/es-es/pycharm/)
+
+[Python 3.9](https://www.python.org/downloads/)
+
+also you can check if already have installed python in your computer with this command line
+
+`python --version`
+
+
+first create a new bot in [discord](https://discord.com/developers/docs/intro)
+
+* Developer Portal --> Applications
+* Click Button --> New Application
+* add a name and create
+* save the Token generated, probably we need later
+* OAuth2 URL Generator -->   **.bot**
+* Check all --> TEXT PERMISSIONS
+* GENERATED URL--> open in a new window and select the server in add server
+* click continue click authorize
+
+
+run this command line in terminal to install discord 
+
+` pip install discord
+`
+
+first configuration to run your own bot 
+
+```
+import discord
+import random
+
+from discord import Client
+
+TOKEN = 'MTAzOTEzNzAyMjE4NDU0MjIyOA.G5Dgys.VFq_EK6yvaPtkj0GqjAILcilbORwP5YAWOwmhc'
+#in this line you need to put the Token created previously for with your bot
+client = discord.Client(intents=discord.Intents.default())
+
+
+@client.event
+async def on_ready():
+    print('we have logged in as {0.user}'.format(client))
+
+
+client.run(TOKEN)
+```
---
+++ bot.py
@@ -0,0 +1,42 @@
+import discord
+import responses
+
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.get_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+
+    except Exception as e:
+        print(e)
+
+
+def run_discord_bot():
+    TOKEN = 'MTAzOTEzNzAyMjE4NDU0MjIyOA.G5Dgys.VFq_EK6yvaPtkj0GqjAILcilbORwP5YAWOwmhc'
+    # in this line you need to put the Token created previously for with your bot
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f'{username} said: "{user_message}"({channel})')
+
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=True)
+
+    client.run(TOKEN)
